8486bc8860d82cb9a4e80dfeff9a4dee3a25f58a
Nick Aralin <nikkofann@gmail.com>
2022-10-27 18:35:32 -0400

    Updates

---
+++ precache-manifest.827cd5c4fbcab608010658ac4fc38690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3901543479ebb18d68e6149c2b985f3e",
+    "url": "/Branch-assignment/index.html"
+  },
+  {
+    "revision": "ec50f85b045e31df6a86",
+    "url": "/Branch-assignment/static/css/main.be011f0a.chunk.css"
+  },
+  {
+    "revision": "5de4b4f0301f0f3dc66f",
+    "url": "/Branch-assignment/static/js/2.379b7ce7.chunk.js"
+  },
+  {
+    "revision": "ec50f85b045e31df6a86",
+    "url": "/Branch-assignment/static/js/main.373fd18b.chunk.js"
+  },
+  {
+    "revision": "e220794a9abfc4d6d520",
+    "url": "/Branch-assignment/static/js/runtime~main.cfbaa97d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ecd6e637a688bdc6519ac89c1749b03.js"
+  "/Branch-assignment/precache-manifest.827cd5c4fbcab608010658ac4fc38690.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Branch-assignment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
