5f44e464b18d51d384406fb8d1a5b68b17ebdc11
Nick Aralin <nikkofann@gmail.com>
2022-10-27 18:28:14 -0400

    Updates

---
+++ precache-manifest.5ecd6e637a688bdc6519ac89c1749b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3aac3f19fe7477405cbdd31d92c09fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec50f85b045e31df6a86",
+    "url": "/static/css/main.be011f0a.chunk.css"
+  },
+  {
+    "revision": "5de4b4f0301f0f3dc66f",
+    "url": "/static/js/2.379b7ce7.chunk.js"
+  },
+  {
+    "revision": "ec50f85b045e31df6a86",
+    "url": "/static/js/main.373fd18b.chunk.js"
+  },
+  {
+    "revision": "4ab288692a2eb4b63a55",
+    "url": "/static/js/runtime~main.93481a9e.js"
+  }
+]);
