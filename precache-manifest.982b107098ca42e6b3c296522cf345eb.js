fe14b7c79ccf17d4a9793f08d170fd5ba65cc72a
Nick Aralin <nikkofann@gmail.com>
2022-10-27 21:22:07 -0400

    Updates

--- precache-manifest.982b107098ca42e6b3c296522cf345eb.js
+++ precache-manifest.982b107098ca42e6b3c296522cf345eb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3901543479ebb18d68e6149c2b985f3e",
+    "revision": "5603fe1a337b1965ac44e20b32d511ab",
     "url": "/Branch-assignment/index.html"
   },
   {
-    "revision": "ec50f85b045e31df6a86",
+    "revision": "abd45a2b8f5492f937bd",
     "url": "/Branch-assignment/static/css/main.be011f0a.chunk.css"
   },
   {
-    "revision": "5de4b4f0301f0f3dc66f",
-    "url": "/Branch-assignment/static/js/2.379b7ce7.chunk.js"
+    "revision": "48aa7b0ebf6b17837b98",
+    "url": "/Branch-assignment/static/js/2.e3681c61.chunk.js"
   },
   {
-    "revision": "ec50f85b045e31df6a86",
-    "url": "/Branch-assignment/static/js/main.373fd18b.chunk.js"
+    "revision": "abd45a2b8f5492f937bd",
+    "url": "/Branch-assignment/static/js/main.2c29e79a.chunk.js"
   },
   {
     "revision": "e220794a9abfc4d6d520",
